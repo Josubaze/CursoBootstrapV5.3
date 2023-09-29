ef3ed863478a0dad423d9c47bd5f9c39e4c837df
Josue Baez <josuejbaeze4@gmail.com>
2023-09-28 22:03:49 -0400

    alertas y ajustes

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+const btn_buys = document.querySelector('#btn-buys');
+
+btn_buys.addEventListener('click', buy_tickets);
+
+function buy_tickets(e){
+  const modal_body = e.target.parentElement.parentElement;
+  const alert = modal_body.querySelector('.alert');
+  alert.classList.remove('inactive');
+}
+
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
